a9d038e19c22a225f4e94dcc1529c5dfa4ec6cb2
agu <agaillard@afip.gob.ar>
2019-03-13 13:58:17 -0300

    Agrega express

--- i.js
+++ i.js
@@ -1,10 +1,37 @@
-const {google} = require('googleapis');
-const books = google.books('v1');
+const express = require('express')
+const google = require('googleapis')
 
-const params = {q: "Harry Potter"}
+const port = process.env.PORT || 3000
+const books = google.books('v1')
 
-books.volumes.list(params).then(res => {
-  console.log(JSON.stringify(res))
-}).catch(error => {
-  console.log(error)
+function resolveAfter2Seconds() {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve('resolved');
+    }, 2000);
+  });
+}
+
+const search = async (q) => {
+  return books.volumes.list({ q })
+}
+
+var app = express()
+app.get('/api/books', async function (req, res) {
+  let q = (req.query.q || "").trim()
+  if (q != "") {
+    try {
+      res.send(JSON.stringify(await search(q)))
+    } catch (e) {
+      res.send(e)
+    }
+  }
+  else
+    res.send("FALTA Q")
 })
+
+// #####################################################################
+
+app.listen(port)
+
+// #####################################################################
